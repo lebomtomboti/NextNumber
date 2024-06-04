2dd872435c3545f1d2f057dedbdcb42d1db9b287
lebomtomboti <lebomtomboti17@gmail.com>
2024-06-04 16:16:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let ranNum = [2,4,6,8,12,14]
+let i = 0
+function nextNum(){
+    if (i<ranNum.length){
+        console.log(ranNum[i])
+        console.log('The index is',i)
+    }
+    i++
+}
+nextNum()
+nextNum()
+nextNum()
+nextNum()
+nextNum()
+nextNum()
